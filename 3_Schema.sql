f7adfb38d3dd83fdcf9b10e0242a1f6fb4cf6ae5
mynavi-hirosuzuki <suzuki.hiroaki@mynavi.jp>
2022-06-25 07:39:23 +0000

    index 緯度経度

--- 3_Schema.sql
+++ 3_Schema.sql
@@ -1,6 +1,8 @@
 
 alter table estate add column d_popularity integer as (-popularity) stored;
 create index estate_idx1 on estate (d_popularity asc, id asc);
+create index estate_idx2 on estate (latitude);
+create index estate_idx3 on estate (longitude);
 
 alter table chair add column d_popularity integer as (-popularity) stored;
 create index chair_idx1 on chair (d_popularity asc, id asc);
